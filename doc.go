57a3b626ae2c2e99322965df2e0291efefaeccb5
M. J. Fromberger <michael.j.fromberger@gmail.com>
2017-12-23 17:36:13 -0800

    Add a note about no-request callers to the package doc.

--- doc.go
+++ doc.go
@@ -189,6 +189,11 @@ NewCaller can also optionally generate a variadic function:
    ...
    prod, err := Mul(cli, 1, 2, 3, 4, 5)
    ...
+
+It can also generate a function with no request parameter:
+
+   Status := jrpc.NewCaller("Status", nil, string("")).(func(*jrpc2.Client) (string, error))
+
 */
 package jrpc2
 
