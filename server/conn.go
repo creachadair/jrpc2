b101e49635c5215f5460e4749eaff490252fe11a
M. J. Fromberger <michael.j.fromberger@gmail.com>
2017-12-30 21:24:03 -0800

    Add server.HTTP and server.InMemory helpers.
    The server.HTTP function adapts a JSON-RPC client to an http.Handler so that
    HTTP requests containing JSON-RPC messages can be processed.
    
    The server.InMemory function constructs an in-memory jrpc2.Server and connects
    a client to it via a pipe.

--- server/conn.go
+++ server/conn.go
@@ -1,7 +1,11 @@
 package server
 
 import (
+	"encoding/json"
+	"io"
+	"io/ioutil"
 	"net"
+	"net/http"
 
 	"bitbucket.org/creachadair/jrpc2"
 )
@@ -9,4 +13,116 @@ import (
 // Listener adapts a net.Listener to an accept function for use with Loop.
 func Listener(lst net.Listener) func() (jrpc2.Conn, error) {
 	return func() (jrpc2.Conn, error) { return lst.Accept() }
+}
+
+// HTTP adapts a *jrpc2.Client to an http.Handler. The body of each HTTP
+// request is transmitted as a JSON-RPC request through the client, and its
+// response is written back as the body of the HTTP reply. Each HTTP request is
+// handled as a synchronous RPC, but arbitrarily-many HTTP requests may be in
+// flight concurrently.
+//
+// If the HTTP request body is empty or malformed, the handler reports status
+// 400 (Bad Request). Any other structural errors in sending or receiving the
+// RPC are reported as status 500 (Internal Server Error). A complete RPC reply
+// reports status 200 (OK) even if the reply contains an error.
+func HTTP(cli *jrpc2.Client) http.Handler {
+	type jerror struct {
+		Code int32           `json:"code"`
+		Msg  string          `json:"message,omitempty"`
+		Data json.RawMessage `json:"data,omitempty"`
+	}
+	type jresponse struct {
+		V  string          `json:"jsonrpc"`
+		ID json.RawMessage `json:"id,omitempty"`
+		E  *jerror         `json:"error,omitempty"`
+		R  json.RawMessage `json:"result,omitempty"`
+	}
+
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		data, err := ioutil.ReadAll(req.Body)
+		if err != nil {
+			http.Error(w, "unable to read request", http.StatusBadRequest)
+			return
+		}
+
+		// Decode the request sufficiently to find the ID, method, and params
+		// so we can forward the request through the client.
+		var parsed struct {
+			ID     json.RawMessage `json:"id,omitempty"`
+			Method string          `json:"method,omitempty"`
+			Params json.RawMessage `json:"params,omitempty"`
+		}
+		if err := json.Unmarshal(data, &parsed); err != nil {
+			http.Error(w, "invalid request: "+err.Error(), http.StatusBadRequest)
+			return
+		}
+		if len(parsed.ID) == 0 {
+			// Send a notification, and reply with an empty success.
+			if err := cli.Notify(parsed.Method, parsed.Params); err != nil {
+				http.Error(w, "sending notification: "+err.Error(), http.StatusInternalServerError)
+
+			}
+			return
+		}
+
+		// Unpack the response and restore the original request ID.
+		rsp, err := cli.CallWait(parsed.Method, parsed.Params)
+		encoded := &jresponse{V: jrpc2.Version, ID: parsed.ID}
+		if err != nil {
+			if e, ok := err.(*jrpc2.Error); ok {
+				encoded.E = &jerror{
+					Code: int32(e.Code),
+					Msg:  e.Message,
+				}
+				e.UnmarshalData(&encoded.E.Data) // cannot fail
+			} else {
+				encoded.E = &jerror{
+					Code: int32(jrpc2.E_InternalError),
+					Msg:  err.Error(),
+					// no data
+				}
+			}
+		} else {
+			rsp.UnmarshalResult(&encoded.R) // cannot fail
+		}
+		if data, err := json.Marshal(encoded); err != nil {
+			http.Error(w, "encoding response failed", http.StatusInternalServerError)
+		} else {
+			w.Header().Set("Content-Type", "application/json")
+			w.Write(data)
+		}
+	})
+}
+
+// InMemory constructs a jrpc2.Server from the specified assigner and server
+// options, and connects an in-memory client to it with the client options.
+func InMemory(assigner jrpc2.Assigner, serverOpt *jrpc2.ServerOptions, clientOpt *jrpc2.ClientOptions) (*jrpc2.Client, error) {
+	cpipe, spipe := newPipe()
+	if _, err := jrpc2.NewServer(assigner, serverOpt).Start(spipe); err != nil {
+		return nil, err
+	}
+	return jrpc2.NewClient(cpipe, clientOpt), nil
+}
+
+// newPipe creates a pair of connected jrpc2.Conn values suitable for wiring
+// together an in-memory client and server. The resulting values are safe for
+// concurrent use by multiple goroutines.
+func newPipe() (client, server jrpc2.Conn) {
+	cr, sw := io.Pipe()
+	sr, cw := io.Pipe()
+	return pipe{PipeReader: cr, PipeWriter: cw}, pipe{PipeReader: sr, PipeWriter: sw}
+}
+
+type pipe struct {
+	*io.PipeReader
+	*io.PipeWriter
+}
+
+func (p pipe) Close() error {
+	rerr := p.PipeReader.Close()
+	werr := p.PipeWriter.Close()
+	if werr != nil {
+		return werr
+	}
+	return rerr
 }
