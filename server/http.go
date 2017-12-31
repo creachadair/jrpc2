dd5e582711f16dd43521743d33046c89c919fdb7
M. J. Fromberger <michael.j.fromberger@gmail.com>
2017-12-31 15:59:08 -0800

    Separate raw call mechanism from HTTP handling.
    Pull out the parts of the HTTP handler code that transmit the request through
    the client, and give that its own identity as Caller. Rework the HTTP helper in
    terms of this wrapper.
    
    The Caller wrapper can be used to proxy requests from any source.

--- server/http.go
+++ server/http.go
@@ -19,44 +19,58 @@ import (
 // RPC are reported as status 500 (Internal Server Error). A complete RPC reply
 // reports status 200 (OK) even if the reply contains an error.
 func HTTP(cli *jrpc2.Client) http.Handler {
+	caller := RawCaller(cli)
 	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 		data, err := ioutil.ReadAll(req.Body)
 		if err != nil {
 			http.Error(w, "unable to read request", http.StatusBadRequest)
 			return
 		}
-
-		// Decode the request sufficiently to find the ID, method, and params
-		// so we can forward the request through the client.
-		var parsed struct {
-			ID     json.RawMessage `json:"id,omitempty"`
-			Method string          `json:"method,omitempty"`
-			Params json.RawMessage `json:"params,omitempty"`
-		}
-		if err := json.Unmarshal(data, &parsed); err != nil {
-			http.Error(w, "invalid request: "+err.Error(), http.StatusBadRequest)
-			return
-		}
-		if len(parsed.ID) == 0 {
-			// Send a notification, and reply with an empty success.
-			if err := cli.Notify(parsed.Method, parsed.Params); err != nil {
-				http.Error(w, "sending notification: "+err.Error(), http.StatusInternalServerError)
-			}
-			return
-		}
-
-		// Unpack the response and restore the original request ID.
-		rsp, err := cli.CallWait(parsed.Method, parsed.Params)
-		if rsp == nil {
+		rsp, err := caller.CallWait(data)
+		if err != nil {
 			http.Error(w, "call failed: "+err.Error(), http.StatusInternalServerError)
 			return
-		} else if data, err := jrpc2.MarshalResponse(rsp, parsed.ID); err != nil {
-			http.Error(w, "encoding response failed: "+err.Error(), http.StatusInternalServerError)
-			return
-		} else {
+		} else if len(rsp) != 0 {
 			w.Header().Set("Content-Type", "application/json")
-			w.Write(data)
+			w.Write(rsp)
 			w.Write([]byte("\n"))
 		}
 	})
+}
+
+// RawCaller returns a wrapper around c that accepts requests as undecoded
+// (raw) JSON and returns replies in the same format.
+func RawCaller(c *jrpc2.Client) Caller { return Caller{cli: c} }
+
+type Caller struct {
+	cli *jrpc2.Client
+}
+
+// CallWait sends a raw JSON-RPC request message through the client and returns
+// the response as plain JSON. The call blocks until complete.  If the request
+// is a notification, CallWait returns nil, nil on success.  Otherwise any
+// successful call, even if it contains an error from the server, reports a
+// complete JSON response message.
+func (c Caller) CallWait(req []byte) ([]byte, error) {
+	// Decode the request sufficiently to find the ID, method, and params
+	// so we can forward the request through the client.
+	var parsed struct {
+		ID     json.RawMessage `json:"id,omitempty"`
+		Method string          `json:"method,omitempty"`
+		Params json.RawMessage `json:"params,omitempty"`
+	}
+	if err := json.Unmarshal(req, &parsed); err != nil {
+		return nil, err
+	} else if len(parsed.ID) == 0 {
+		// Send a notification, and reply with an empty success.
+		return nil, c.cli.Notify(parsed.Method, parsed.Params)
+	}
+	rsp, err := c.cli.CallWait(parsed.Method, parsed.Params)
+	if rsp == nil {
+		// We only need to directly report the error in case it prevented us
+		// from getting a response at all; errors within the protocol are
+		// encoded in the response directly.
+		return nil, err
+	}
+	return jrpc2.MarshalResponse(rsp, parsed.ID)
 }
