7764baa39bb79d26ee985961334db8037c7c0e6e
M. J. Fromberger <michael.j.fromberger@gmail.com>
2018-01-06 21:07:02 -0800

    Make constructors return interface type.
    Returning jrpc2.Channel allows these constructors to be used in the places
    where a conversion from connection to channel is required, e.g., server.Loop.

--- channel/pipe.go
+++ channel/pipe.go
@@ -1,9 +1,13 @@
 package channel
 
-import "io"
+import (
+	"io"
+
+	"bitbucket.org/creachadair/jrpc2"
+)
 
 // Pipe creates a pair of connected in-memory raw channels.
-func Pipe() (client, server Raw) {
+func Pipe() (client, server jrpc2.Channel) {
 	cr, sw := io.Pipe()
 	sr, cw := io.Pipe()
 	client = NewRaw(pipe{cr, cw})
--- channel/raw.go
+++ channel/raw.go
@@ -3,11 +3,13 @@ package channel
 import (
 	"encoding/json"
 	"io"
+
+	"bitbucket.org/creachadair/jrpc2"
 )
 
 // NewRaw constructs a jrpc2.Channel that transmits and receives messages on
 // rwc with no explicit framing.
-func NewRaw(rwc io.ReadWriteCloser) Raw { return Raw{rwc: rwc, dec: json.NewDecoder(rwc)} }
+func NewRaw(rwc io.ReadWriteCloser) jrpc2.Channel { return Raw{rwc: rwc, dec: json.NewDecoder(rwc)} }
 
 // Raw implements jrpc2.Channel. Messages sent on a Raw channel are not
 // explicitly framed, and messages received are framed by JSON syntax.
