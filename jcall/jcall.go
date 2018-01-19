2cce780c1f51ebbf3a9fe58577715a058da55db8
M. J. Fromberger <michael.j.fromberger@gmail.com>
2018-01-18 23:21:25 -0800

    Allow multiple calls per invocation.

--- jcall/jcall.go
+++ jcall/jcall.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"os"
 
 	"bitbucket.org/creachadair/jrpc2"
 	"bitbucket.org/creachadair/jrpc2/channel"
@@ -22,37 +23,61 @@ var (
 func main() {
 	flag.Parse()
 
-	if n := flag.NArg(); n < 2 || n > 3 {
-		log.Fatal("Arguments are <address> <method> [<params>]")
+	// There must be at least one request, and more are permitted.  Each method
+	// must have an argument, though it may be empty.
+	if flag.NArg() < 3 || flag.NArg()%2 == 0 {
+		log.Fatal("Arguments are <address> {<method> <params>}...")
 	}
-	addr, method := flag.Arg(0), flag.Arg(1)
-	var params interface{}
-	if flag.NArg() == 3 {
-		params = json.RawMessage(flag.Arg(2))
+	addr := flag.Arg(0)
+	specs := make([]jrpc2.Spec, flag.NArg()/2)
+	for i, j := 1, 0; i < flag.NArg(); i += 2 {
+		specs[j].Method = flag.Arg(i)
+		if p := flag.Arg(i + 1); p != "" {
+			specs[j].Params = json.RawMessage(p)
+		}
+		j++
 	}
 
+	// Connect to the server and establish a client.
 	conn, err := net.Dial("tcp", addr)
 	if err != nil {
 		log.Fatalf("Dial %q: %v", addr, err)
 	}
 	defer conn.Close()
 	cli := jrpc2.NewClient(channel.Raw(conn), nil)
+
+	// Handle notifications...
 	if *doNotify {
-		if err := cli.Notify(method, params); err != nil {
-			log.Fatalf("Notify failed: %v", err)
+		for _, spec := range specs {
+			if err := cli.Notify(spec.Method, spec.Params); err != nil {
+				log.Fatalf("Notify %q failed: %v", spec.Method, err)
+			}
 		}
 		return
 	}
 
-	rsp, err := cli.CallWait(method, params)
+	// Handle a batch of requests.
+	batch, err := cli.Batch(specs)
 	if err != nil {
 		log.Fatalf("Call failed: %v", err)
-	} else if rerr := rsp.Error(); rerr != nil {
-		log.Fatalf("Error: %v", rerr)
 	}
-	var result json.RawMessage
-	if err := rsp.UnmarshalResult(&result); err != nil {
-		log.Fatalf("Decoding result: %v", err)
+	rsps := batch.Wait()
+	failed := false
+	for i, rsp := range rsps {
+		if rerr := rsp.Error(); rerr != nil {
+			log.Printf("Error (%d): %v", i+1, rerr)
+			failed = true
+			continue
+		}
+		var result json.RawMessage
+		if err := rsp.UnmarshalResult(&result); err != nil {
+			log.Printf("Decoding (%d): %v", i+1, err)
+			failed = true
+			continue
+		}
+		fmt.Println(string(result))
 	}
-	fmt.Println(string(result))
+	if failed {
+		os.Exit(1)
+	}
 }
