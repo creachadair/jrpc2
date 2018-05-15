f2d3b54d507ef5a4314a4ee2673106a885c3a51e
M. J. Fromberger <michael.j.fromberger@gmail.com>
2018-05-14 19:35:09 -0700

    Consolidate the send/receive tests for pipes.

--- channel/pipe_test.go
+++ channel/pipe_test.go
@@ -5,60 +5,50 @@ import (
 	"testing"
 )
 
+type sender interface {
+	Send([]byte) error
+}
+type receiver interface {
+	Recv() ([]byte, error)
+}
+
+func testSendRecv(t *testing.T, s sender, r receiver, msg string) {
+	var wg sync.WaitGroup
+	var sendErr, recvErr error
+	var data []byte
+
+	wg.Add(2)
+	go func() {
+		defer wg.Done()
+		sendErr = s.Send([]byte(msg))
+	}()
+	go func() {
+		defer wg.Done()
+		data, recvErr = r.Recv()
+	}()
+	wg.Wait()
+
+	if sendErr != nil {
+		t.Errorf("Send(%q): unexpected error: %v", msg, sendErr)
+	}
+	if recvErr != nil {
+		t.Errorf("Recv(): unexpected error: %v", recvErr)
+	}
+	if got := string(data); got != msg {
+		t.Errorf("Recv(): got %q, want %q", got, msg)
+	}
+}
+
 func TestPipe(t *testing.T) {
 	lhs, rhs := Pipe()
 	defer lhs.Close()
 	defer rhs.Close()
 
 	const message1 = `["Full plate and packing steel"]`
-
-	var wg sync.WaitGroup
-	var lhsSendErr, lhsRecvErr, rhsSendErr, rhsRecvErr error
-	var lhsgot, rhsgot []byte
-
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		lhsSendErr = lhs.Send([]byte(message1))
-	}()
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		rhsgot, rhsRecvErr = rhs.Recv()
-	}()
-	wg.Wait()
-
-	if lhsSendErr != nil {
-		t.Errorf("Send (left): %v", lhsSendErr)
-	}
-	if rhsRecvErr != nil {
-		t.Errorf("Recv (right): %v", rhsRecvErr)
-	}
-	if got, want := string(rhsgot), message1; got != want {
-		t.Errorf("Message (right): got %#q, want %#q", got, want)
-	}
-
 	const message2 = `{"slogan":"Jump on your sword, evil!"}`
 
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		rhsSendErr = rhs.Send([]byte(message2))
-	}()
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		lhsgot, lhsRecvErr = lhs.Recv()
-	}()
-	wg.Wait()
-
-	if rhsSendErr != nil {
-		t.Errorf("Send (right): %v", rhsSendErr)
-	}
-	if lhsRecvErr != nil {
-		t.Errorf("Recv (left): %v", lhsRecvErr)
-	}
-	if got, want := string(lhsgot), message2; got != want {
-		t.Errorf("Message (left): got %#q, want %#q", got, want)
-	}
+	t.Logf("Testing lhs ⇒ rhs :: %q", message1)
+	testSendRecv(t, lhs, rhs, message1)
+	t.Logf("Testing rhs ⇒ lhs :: %q", message2)
+	testSendRecv(t, rhs, lhs, message2)
 }
