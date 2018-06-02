2803fe6cce0f8f49cd30377abfa42d87bb6e869e
M. J. Fromberger <michael.j.fromberger@gmail.com>
2018-06-01 23:11:25 -0700

    Improve the doc comments for package channel.
    Remove a reference to jrpc2.Channel.
    Clarify the behaviour of Send and Receive.

--- channel/doc.go
+++ channel/doc.go
@@ -1,4 +1,6 @@
-// Package channel defines implementations of the jrpc2.Channel interface.
+// Package channel defines a communications channel that can encode/transmit
+// and decode/receive data records with a configurable framing discipline, and
+// provides some simple framing implementations.
 package channel
 
 // A Channel represents the ability to transmit and receive data records.  A
@@ -6,11 +8,13 @@ package channel
 // framing so that records can be embedded in higher-level protocols.  The
 // methods of a Channel need not be safe for concurrent use.
 type Channel interface {
-	// Send transmits a record on the channel.
+	// Send transmits a record on the channel. Each call to Send transmits one
+	// complete record.
 	Send([]byte) error
 
 	// Recv returns the next available record from the channel.  If no further
-	// messages are available, it returns io.EOF.
+	// messages are available, it returns io.EOF.  Each call to Recv fetches a
+	// single complete record.
 	Recv() ([]byte, error)
 
 	// Close shuts down the channel, after which no further records may be
