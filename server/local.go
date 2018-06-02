245a448152d185144fefc601b4638c9c17b5361f
M. J. Fromberger <michael.j.fromberger@gmail.com>
2018-06-01 23:46:43 -0700

    Give server.Local its own options type.

--- server/local.go
+++ server/local.go
@@ -5,10 +5,21 @@ import (
 	"bitbucket.org/creachadair/jrpc2/channel"
 )
 
-// Local constructs a jrpc2.Server from the specified assigner and server
-// options, and connects an in-memory client to it with the client options.
-func Local(assigner jrpc2.Assigner, serverOpt *jrpc2.ServerOptions, clientOpt *jrpc2.ClientOptions) *jrpc2.Client {
+// Local constructs a jrpc2.Server from the specified assigner and options.
+// If opts == nil, it behaves as if the client and server options are also nil.
+// When the client is closed, the server is also stopped.
+func Local(assigner jrpc2.Assigner, opts *LocalOptions) *jrpc2.Client {
+	if opts == nil {
+		opts = new(LocalOptions)
+	}
 	cpipe, spipe := channel.Pipe(channel.Line)
-	jrpc2.NewServer(assigner, serverOpt).Start(spipe)
-	return jrpc2.NewClient(cpipe, clientOpt)
+	jrpc2.NewServer(assigner, opts.ServerOptions).Start(spipe)
+	return jrpc2.NewClient(cpipe, opts.ClientOptions)
+}
+
+// LocalOptions control the behaviour of the server and client constructed by
+// the Local function.
+type LocalOptions struct {
+	ClientOptions *jrpc2.ClientOptions
+	ServerOptions *jrpc2.ServerOptions
 }
