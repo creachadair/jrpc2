bd7479d6643e448a5b4acfd3123ebf97a40c17f7
M. J. Fromberger <michael.j.fromberger@gmail.com>
2018-06-06 19:22:55 -0700

    Move context handlers to their own file.
    Pull out the context keys and the helper functions that use them into their own
    file within the jrpc2 package.

---
+++ ctx.go
@@ -0,0 +1,53 @@
+package jrpc2
+
+import (
+	"context"
+
+	"bitbucket.org/creachadair/jrpc2/metrics"
+)
+
+// requestContextKey is the concrete type of the context key used to dispatch
+// the request context in to handlers.
+type requestContextKey string
+
+// ServerMetrics returns the server metrics collector associated with the given
+// context, or nil if ctx doees not have a collector attached.  The context
+// passed to a handler by *jrpc2.Server will include this value.
+func ServerMetrics(ctx context.Context) *metrics.M {
+	if v := ctx.Value(serverMetricsKey); v != nil {
+		return v.(*metrics.M)
+	}
+	return nil
+}
+
+const serverMetricsKey = requestContextKey("server-metrics")
+
+// InboundRequest returns the inbound request associated with the given
+// context, or nil if ctx does not have an inbound request. The context passed
+// to the handler by *jrpc2.Server will include this value.
+//
+// This is mainly useful to wrapped server methods that do not have the request
+// as an explicit parameter; for direct implementations of Method.Call the
+// request value returned by InboundRequest will be the same value as was
+// passed explicitly.
+func InboundRequest(ctx context.Context) *Request {
+	if v := ctx.Value(inboundRequestKey); v != nil {
+		return v.(*Request)
+	}
+	return nil
+}
+
+const inboundRequestKey = requestContextKey("inbound-request")
+
+// ServerNotify returns the server notifier associated with the given context,
+// or nil if ctx does not have a server notifier. The context passed to the
+// handler by *jrpc2.Server will include this value if the server was
+// constructed with the AllowNotify option set true.
+func ServerNotify(ctx context.Context) func(context.Context, string, interface{}) error {
+	if v := ctx.Value(serverNotifyKey); v != nil {
+		return v.(func(context.Context, string, interface{}) error)
+	}
+	return nil
+}
+
+const serverNotifyKey = requestContextKey("server-notify")
--- server.go
+++ server.go
@@ -526,36 +526,3 @@ func (ts tasks) responses() jresponses {
 	}
 	return rsps
 }
-
-// InboundRequest returns the inbound request associated with the given
-// context, or nil if ctx does not have an inbound request. The context passed
-// to the handler by *jrpc2.Server will include this value.
-//
-// This is mainly useful to wrapped server methods that do not have the request
-// as an explicit parameter; for direct implementations of Method.Call the
-// request value returned by InboundRequest will be the same value as was
-// passed explicitly.
-func InboundRequest(ctx context.Context) *Request {
-	if v := ctx.Value(inboundRequestKey); v != nil {
-		return v.(*Request)
-	}
-	return nil
-}
-
-// ServerNotify returns the server notifier associated with the given context,
-// or nil if ctx does not have a server notifier. The context passed to the
-// handler by *jrpc2.Server will include this value if the server was
-// constructed with the AllowNotify option set true.
-func ServerNotify(ctx context.Context) func(context.Context, string, interface{}) error {
-	if v := ctx.Value(serverNotifyKey); v != nil {
-		return v.(func(context.Context, string, interface{}) error)
-	}
-	return nil
-}
-
-// requestContextKey is the concrete type of the context key used to dispatch
-// the request context in to handlers.
-type requestContextKey string
-
-const inboundRequestKey = requestContextKey("inbound-request")
-const serverNotifyKey = requestContextKey("server-notify")
