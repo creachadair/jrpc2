1cb723f6769eddae3d7d79be5f7e6385ef3207b6
M. J. Fromberger <michael.j.fromberger@gmail.com>
2018-06-12 17:31:12 -0700

    Add a unit test for channel.WithTrigger.

--- channel/chan_test.go
+++ channel/chan_test.go
@@ -1,6 +1,9 @@
 package channel
 
-import "testing"
+import (
+	"io"
+	"testing"
+)
 
 var tests = []struct {
 	name    string
@@ -41,6 +44,46 @@ func TestEmptyMessage(t *testing.T) {
 
 			t.Log(`Testing lhs → rhs :: "" (empty line)`)
 			testSendRecv(t, lhs, rhs, "")
+		})
+	}
+}
+
+func TestWithTrigger(t *testing.T) {
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			r, w := io.Pipe()
+			triggered := false
+			ch := WithTrigger(test.framing(r, w), func() {
+				triggered = true
+			})
+
+			// Send a message to the channel, then close it.
+			const message = `["fools", "rush", "in"]`
+			go func() {
+				t.Log("Sending...")
+				if err := ch.Send([]byte(message)); err != nil {
+					t.Errorf("Send failed: %v", err)
+				}
+				t.Logf("Close: err=%v", ch.Close())
+			}()
+
+			// Read messages from the channel till it closes, then check that
+			// the trigger was correctly invoked.
+			for {
+				msg, err := ch.Recv()
+				if err == io.EOF {
+					t.Log("Recv: returned io.EOF")
+					break
+				} else if err != nil {
+					t.Errorf("Recv: unexpected error: %v", err)
+					break
+				}
+				t.Logf("Recv: msg=%q", string(msg))
+			}
+
+			if !triggered {
+				t.Error("After channel close: trigger not called")
+			}
 		})
 	}
 }
