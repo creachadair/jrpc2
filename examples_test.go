8e7b08207a82e9d673f5fbe1ec647122090fcb95
M. J. Fromberger <michael.j.fromberger@gmail.com>
2018-06-15 22:38:20 -0700

    Add an example for calling Batch.

--- examples_test.go
+++ examples_test.go
@@ -18,11 +18,19 @@ var (
 	c        = jrpc2.NewClient(cli, nil)
 )
 
+type Msg struct {
+	Text string `json:"msg"`
+}
+
 func ExampleNewServer() {
 	// Construct a new server with a single method "Hello".
 	s = jrpc2.NewServer(jrpc2.MapAssigner{
 		"Hello": jrpc2.NewMethod(func(ctx context.Context) (string, error) {
 			return "Hello, world!", nil
+		}),
+		"Log": jrpc2.NewMethod(func(ctx context.Context, msg Msg) (bool, error) {
+			fmt.Println("Log:", msg.Text)
+			return true, nil
 		}),
 	}, nil).Start(srv)
 
@@ -33,6 +41,7 @@ func ExampleNewServer() {
 	fmt.Println(strings.Join(si.Methods, "\n"))
 	// Output:
 	// Hello
+	// Log
 }
 
 func ExampleClient_Call() {
@@ -48,4 +57,34 @@ func ExampleClient_Call() {
 	fmt.Println(msg)
 	// Output:
 	// Hello, world!
+}
+
+func ExampleClient_Batch() {
+	// var c = jrpc2.NewClient(cli, nil)
+	b, err := c.Batch(ctx, []jrpc2.Spec{
+		{Method: "Hello"},
+		{Method: "Log", Params: Msg{"Sing it!"}, Notify: true},
+	})
+	if err != nil {
+		log.Fatalf("Batch: %v", err)
+	}
+
+	// Wait for all the replies. There should be only one in this case, since
+	// we sent 1 notification and 1 request.
+	rsps := b.Wait()
+	if len(rsps) != 1 {
+		log.Fatalf("Wait: got %d responses, wanted 1", len(rsps))
+	}
+	fmt.Printf("len(rsps)=%d\n", len(rsps))
+
+	// Decode the result from the request.
+	var msg string
+	if err := rsps[0].UnmarshalResult(&msg); err != nil {
+		log.Fatalf("Invalid result: %v", err)
+	}
+	fmt.Printf("rsps[0]=%s\n", msg)
+	// Output:
+	// Log: Sing it!
+	// len(rsps)=1
+	// rsps[0]=Hello, world!
 }
