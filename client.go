28c7bb1687075297e3eb088f5a1104344ab164bd
M. J. Fromberger <michael.j.fromberger@gmail.com>
2018-06-25 19:51:00 -0700

    Run gofmt.

--- client.go
+++ client.go
@@ -42,7 +42,7 @@ func NewClient(ch channel.Channel, opts *ClientOptions) *Client {
 		// Lock-protected fields
 		ch:      ch,
 		pending: make(map[string]*Response),
-		nextID: 1,
+		nextID:  1,
 
 		// Note that we start the ID counter at 1 here to avoid issues with a
 		// server implementation that treats 0 as equivalent to null.
