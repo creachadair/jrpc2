216fdabea4169839e0f5618a5b30796b7fdfb533
M. J. Fromberger <michael.j.fromberger@gmail.com>
2018-07-07 11:34:45 -0700

    Pull out common code for length-prefixed framings.
    Separate the encoding and decoding into functions, and share the plumbing for
    the output buffer and buffered reader.

--- channel/decimal.go
+++ channel/decimal.go
@@ -2,7 +2,6 @@ package channel
 
 import (
 	"bufio"
-	"bytes"
 	"io"
 	"strconv"
 )
@@ -16,43 +15,23 @@ import (
 //    empanada\n
 //
 func Decimal(r io.Reader, wc io.WriteCloser) Channel {
-	return &decimal{wc: wc, rd: bufio.NewReader(r), buf: bytes.NewBuffer(nil)}
+	ch := newLenPrefix(r, wc)
+	ch.enc = encodeLenDecimal
+	ch.dec = decodeLenDecimal
+	return ch
 }
 
-// A decimal implements Channel. Messages sent on a varint channel are framed
-// with a decimal-encoded length on a line by itself.
-type decimal struct {
-	wc  io.WriteCloser
-	rd  *bufio.Reader
-	buf *bytes.Buffer
-}
-
-// Send implements part of the Channel interface.
-func (d *decimal) Send(msg []byte) error {
-	d.buf.Reset()
-	d.buf.WriteString(strconv.Itoa(len(msg)))
-	d.buf.WriteByte('\n')
-	d.buf.Write(msg)
-	_, err := d.wc.Write(d.buf.Next(d.buf.Len()))
+func encodeLenDecimal(n int, w io.Writer) error {
+	_, err := io.WriteString(w, strconv.Itoa(n)+"\n")
 	return err
 }
 
-// Recv implements part of the Channel interface.
-func (d *decimal) Recv() ([]byte, error) {
-	pfx, err := d.rd.ReadString('\n')
+func decodeLenDecimal(rd *bufio.Reader) (int, error) {
+	pfx, err := rd.ReadString('\n')
 	if err == io.EOF && pfx != "" {
 		// handle a partial line at EOF
 	} else if err != nil {
-		return nil, err
+		return 0, err
 	}
-	ln, err := strconv.Atoi(pfx[:len(pfx)-1])
-	if err != nil {
-		return nil, err
-	}
-	out := make([]byte, int(ln))
-	nr, err := io.ReadFull(d.rd, out)
-	return out[:nr], err
+	return strconv.Atoi(pfx[:len(pfx)-1])
 }
-
-// Close implements part of the Channel interface.
-func (d *decimal) Close() error { return d.wc.Close() }
--- channel/varint.go
+++ channel/varint.go
@@ -2,50 +2,31 @@ package channel
 
 import (
 	"bufio"
-	"bytes"
 	"encoding/binary"
 	"io"
-	"math/bits"
 )
 
 // Varint is a framing that transmits and receives messages on r and wc, with
 // each message prefixed by its length encoded in a varint as defined by the
 // encoding/binary package.
 func Varint(r io.Reader, wc io.WriteCloser) Channel {
-	return &varint{wc: wc, rd: bufio.NewReader(r), buf: bytes.NewBuffer(nil)}
+	ch := newLenPrefix(r, wc)
+	ch.enc = encodeLenVarint
+	ch.dec = decodeLenVarint
+	return ch
 }
 
-// A varint implements Channel. Messages sent on a varint channel are framed
-// with a varint length prefix.
-type varint struct {
-	wc  io.WriteCloser
-	rd  *bufio.Reader
-	buf *bytes.Buffer
-}
-
-func varintLen(n int) int { return (bits.Len64(uint64(n)) + 6) / 7 }
-
-// Send implements part of the Channel interface.
-func (c *varint) Send(msg []byte) error {
+func encodeLenVarint(n int, w io.Writer) error {
 	var ln [binary.MaxVarintLen64]byte
-	n := binary.PutUvarint(ln[:], uint64(len(msg)))
-	c.buf.Reset()
-	c.buf.Write(ln[:n])
-	c.buf.Write(msg)
-	_, err := c.wc.Write(c.buf.Next(c.buf.Len()))
+	nb := binary.PutUvarint(ln[:], uint64(n))
+	_, err := w.Write(ln[:nb])
 	return err
 }
 
-// Recv implements part of the Channel interface.
-func (c *varint) Recv() ([]byte, error) {
-	ln, err := binary.ReadUvarint(c.rd)
+func decodeLenVarint(rd *bufio.Reader) (int, error) {
+	ln, err := binary.ReadUvarint(rd)
 	if err != nil {
-		return nil, err
+		return 0, err
 	}
-	out := make([]byte, int(ln))
-	nr, err := io.ReadFull(c.rd, out)
-	return out[:nr], err
+	return int(ln), nil
 }
-
-// Close implements part of the Channel interface.
-func (c *varint) Close() error { return c.wc.Close() }
