0cc65bf09eabc4ad7feddeb1f3008f46d4428a25
M. J. Fromberger <michael.j.fromberger@gmail.com>
2018-11-30 15:48:35 -0800

    chanutil: Add "header:t" for a MIME-header channel.

--- channel/chanutil/chanutil.go
+++ channel/chanutil/chanutil.go
@@ -2,19 +2,29 @@
 // framing defined by the bitbucket.org/creachadair/jrpc2/channel package.
 package chanutil
 
-import "bitbucket.org/creachadair/jrpc2/channel"
+import (
+	"strings"
+
+	"bitbucket.org/creachadair/jrpc2/channel"
+)
 
 // Framing returns a channel.Framing described by the specified name, or nil if
 // the name is unknown. The framing types currently understood are:
 //
-//    decimal -- corresponds to channel.Decimal
-//    json    -- corresponds to channel.JSON
-//    line    -- corresponds to channel.Line
-//    lsp     -- corresponds to channel.LSP
-//    raw     -- corresponds to channel.RawJSON
-//    varint  -- corresponds to channel.Varint
+//    decimal  -- corresponds to channel.Decimal
+//    header:t -- corresponds to channel.Header(t)
+//    json     -- corresponds to channel.JSON
+//    line     -- corresponds to channel.Line
+//    lsp      -- corresponds to channel.LSP
+//    raw      -- corresponds to channel.RawJSON
+//    varint   -- corresponds to channel.Varint
 //
-func Framing(name string) channel.Framing { return framings[name] }
+func Framing(name string) channel.Framing {
+	if t := strings.TrimPrefix(name, "header:"); t != name {
+		return channel.Header(t)
+	}
+	return framings[name]
+}
 
 var framings = map[string]channel.Framing{
 	"decimal": channel.Decimal,
