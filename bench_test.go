f3c4e691a57fbc2c362b521f06f1c7dddc029e94
M. J. Fromberger <michael.j.fromberger@gmail.com>
2019-01-27 11:08:05 -0800

    Add a basic round-trip benchmark for client/server overhead.

---
+++ bench_test.go
@@ -0,0 +1,34 @@
+package jrpc2_test
+
+import (
+	"context"
+	"testing"
+
+	"bitbucket.org/creachadair/jrpc2"
+	"bitbucket.org/creachadair/jrpc2/server"
+)
+
+func BenchmarkRoundTrip(b *testing.B) {
+	// Benchmark the round-trip call cycle for a method that does no useful
+	// work, as a proxy for overhead for client and server maintenance.
+	cli, wait := server.Local(jrpc2.MapAssigner{
+		"void": jrpc2.NewHandler(func(context.Context, *jrpc2.Request) (interface{}, error) {
+			return nil, nil
+		}),
+	}, &server.LocalOptions{
+		ServerOptions: &jrpc2.ServerOptions{
+			DisableBuiltin: true,
+			Concurrency:    1,
+		},
+	})
+	defer wait()
+	defer cli.Close()
+	ctx := context.Background()
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		if _, err := cli.Call(ctx, "void", nil); err != nil {
+			b.Fatalf("Call void failed: %v", err)
+		}
+	}
+}
