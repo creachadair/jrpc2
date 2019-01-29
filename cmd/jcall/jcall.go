40391f0a597b24af8208d735a7b046037943f42b
M. J. Fromberger <michael.j.fromberger@gmail.com>
2019-01-28 18:55:36 -0800

    jcall: Add NUL to the list of framing advice.

--- cmd/jcall/jcall.go
+++ cmd/jcall/jcall.go
@@ -52,6 +52,7 @@ server in order for communication to work. The options are:
   json       -- header-framed, content-type application/json
   line       -- byte-terminated, records end in LF (Unicode 10)
   lsp        -- header-framed, content-type application/vscode-jsonrpc (like LSP)
+  nul        -- byte-terminated, records end in NUL (Unicode 0)
   raw        -- unframed, each message is a complete JSON value
   varint     -- length-prefixed, length is a binary varint
 
