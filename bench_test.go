da87a04e2b91d2f82c53624c993d292561948818
M. J. Fromberger <michael.j.fromberger@gmail.com>
2019-06-03 13:27:08 -0700

    Add benchmarks for more config variations.
    +/- B   with or without builtin support
    +/- CTX with or without context support
    Cnn     allow nn concurrent handlers
    
    The variations do not make a substantial difference, though adding context
    support costs 2-3 microseconds per transaction.

--- bench_test.go
+++ bench_test.go
@@ -6,29 +6,65 @@ import (
 
 	"bitbucket.org/creachadair/jrpc2"
 	"bitbucket.org/creachadair/jrpc2/handler"
+	"bitbucket.org/creachadair/jrpc2/jctx"
 	"bitbucket.org/creachadair/jrpc2/server"
 )
 
 func BenchmarkRoundTrip(b *testing.B) {
 	// Benchmark the round-trip call cycle for a method that does no useful
 	// work, as a proxy for overhead for client and server maintenance.
-	loc := server.NewLocal(handler.Map{
-		"void": handler.New(func(context.Context, *jrpc2.Request) (interface{}, error) {
+	voidService := handler.Map{
+		"void": handler.Func(func(context.Context, *jrpc2.Request) (interface{}, error) {
 			return nil, nil
 		}),
-	}, &server.LocalOptions{
-		Server: &jrpc2.ServerOptions{
-			DisableBuiltin: true,
-			Concurrency:    1,
-		},
-	})
-	defer loc.Close()
-	ctx := context.Background()
+	}
+	ctxClient := &jrpc2.ClientOptions{EncodeContext: jctx.Encode}
+	tests := []struct {
+		desc string
+		cli  *jrpc2.ClientOptions
+		srv  *jrpc2.ServerOptions
+	}{
+		{"C01-CTX-B", nil, &jrpc2.ServerOptions{DisableBuiltin: true, Concurrency: 1}},
+		{"C01-CTX+B", nil, &jrpc2.ServerOptions{Concurrency: 1}},
+		{"C04-CTX-B", nil, &jrpc2.ServerOptions{DisableBuiltin: true, Concurrency: 4}},
+		{"C04-CTX+B", nil, &jrpc2.ServerOptions{Concurrency: 4}},
+		{"C12-CTX-B", nil, &jrpc2.ServerOptions{DisableBuiltin: true, Concurrency: 12}},
+		{"C12-CTX+B", nil, &jrpc2.ServerOptions{Concurrency: 12}},
 
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		if _, err := loc.Client.Call(ctx, "void", nil); err != nil {
-			b.Fatalf("Call void failed: %v", err)
-		}
+		{"C01+CTX-B", ctxClient,
+			&jrpc2.ServerOptions{DecodeContext: jctx.Decode, DisableBuiltin: true, Concurrency: 1},
+		},
+		{"C01+CTX+B", ctxClient,
+			&jrpc2.ServerOptions{DecodeContext: jctx.Decode, Concurrency: 1},
+		},
+		{"C04+CTX-B", ctxClient,
+			&jrpc2.ServerOptions{DecodeContext: jctx.Decode, DisableBuiltin: true, Concurrency: 4},
+		},
+		{"C04+CTX+B", ctxClient,
+			&jrpc2.ServerOptions{DecodeContext: jctx.Decode, Concurrency: 4},
+		},
+		{"C12+CTX-B", ctxClient,
+			&jrpc2.ServerOptions{DecodeContext: jctx.Decode, DisableBuiltin: true, Concurrency: 4},
+		},
+		{"C12+CTX+B", ctxClient,
+			&jrpc2.ServerOptions{DecodeContext: jctx.Decode, Concurrency: 12},
+		},
+	}
+	for _, test := range tests {
+		b.Run(test.desc, func(b *testing.B) {
+			loc := server.NewLocal(voidService, &server.LocalOptions{
+				Client: test.cli,
+				Server: test.srv,
+			})
+			defer loc.Close()
+			ctx := context.Background()
+
+			b.ResetTimer()
+			for i := 0; i < b.N; i++ {
+				if _, err := loc.Client.Call(ctx, "void", nil); err != nil {
+					b.Fatalf("Call void failed: %v", err)
+				}
+			}
+		})
 	}
 }
