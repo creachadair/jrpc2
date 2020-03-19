e02e7b995cc479e7072cc839beb2058ba65d262e
M. J. Fromberger <michael.j.fromberger@gmail.com>
2020-03-19 15:07:55 -0700

    Add a session state service test.

--- server/loop_test.go
+++ server/loop_test.go
@@ -15,11 +15,50 @@ import (
 
 var newChan = channel.Varint
 
+// A static test service that returns the same thing each time.
 var testService = NewStatic(handler.Map{
 	"Test": handler.New(func(context.Context) (string, error) {
 		return "OK", nil
 	}),
 })
+
+// A service with session state, to exercise start/finish plumbing.
+type testSession struct {
+	t     *testing.T
+	init  bool
+	nCall int
+	stat  jrpc2.ServerStatus
+}
+
+func newTestSession(t *testing.T) func() Service {
+	return func() Service { t.Helper(); return &testSession{t: t} }
+}
+
+func (t *testSession) Assigner() (jrpc2.Assigner, error) {
+	if t.init {
+		t.t.Error("Service has already been initalized")
+	}
+	t.init = true
+	return handler.Map{
+		"Test": handler.New(func(context.Context) (string, error) {
+			if !t.init {
+				t.t.Error("Handler called before service initialized")
+			}
+			t.nCall++
+			return "OK", nil
+		}),
+	}, nil
+}
+
+func (t *testSession) Finish(stat jrpc2.ServerStatus) {
+	if !t.init {
+		t.t.Error("Service finished without being initialized")
+	}
+	if !stat.Success() {
+		t.t.Errorf("Finish unsuccessful: %v", stat.Err)
+	}
+	t.t.Logf("Server finished after %d calls, err=%v", t.nCall, stat.Err)
+}
 
 func mustListen(t *testing.T) net.Listener {
 	t.Helper()
@@ -80,38 +119,52 @@ func TestSeq(t *testing.T) {
 
 // Test that concurrent clients against the same server work sanely.
 func TestLoop(t *testing.T) {
-	lst := mustListen(t)
-	defer lst.Close()
-	addr := lst.Addr().String()
-	sc := mustServe(t, lst, testService)
-
-	// Start a bunch of clients, each of which will dial the server and make
-	// some calls at random intervals to tickle the race detector.
-	var wg sync.WaitGroup
-	for i := 0; i < 5; i++ {
-		wg.Add(1)
-		i := i
-		go func() {
-			defer wg.Done()
-			cli := mustDial(t, addr)
-			defer cli.Close()
-
-			for j := 0; j < 5; j++ {
-				time.Sleep(time.Duration(rand.Intn(10)) * time.Millisecond)
-				var rsp string
-				if err := cli.CallResult(context.Background(), "Test", nil, &rsp); err != nil {
-					t.Errorf("[client %d]: Test call %d: unexpected error: %v", i, j+1, err)
-				} else if rsp != "OK" {
-					t.Errorf("[client %d]: Test call %d: got %q, want OK", i, j+1, rsp)
-				}
-			}
-		}()
+	tests := []struct {
+		desc string
+		cons func() Service
+	}{
+		{"StaticService", testService},
+		{"SessionStateService", newTestSession(t)},
 	}
+	const numClients = 5
+	const numCalls = 5
 
-	// Wait for the clients to be finished and then close the listener so that
-	// the service loop will stop.
-	wg.Wait()
-	t.Log("Clients are finished; closing listener")
-	lst.Close()
-	<-sc
+	for _, test := range tests {
+		t.Run(test.desc, func(t *testing.T) {
+			lst := mustListen(t)
+			addr := lst.Addr().String()
+			sc := mustServe(t, lst, test.cons)
+
+			// Start a bunch of clients, each of which will dial the server and make
+			// some calls at random intervals to tickle the race detector.
+			var wg sync.WaitGroup
+			for i := 0; i < numClients; i++ {
+				wg.Add(1)
+				i := i
+				go func() {
+					defer wg.Done()
+					cli := mustDial(t, addr)
+					defer cli.Close()
+
+					for j := 0; j < numCalls; j++ {
+						time.Sleep(time.Duration(rand.Intn(10)) * time.Millisecond)
+						var rsp string
+						if err := cli.CallResult(context.Background(), "Test", nil, &rsp); err != nil {
+							t.Errorf("[client %d]: Test call %d: unexpected error: %v", i, j+1, err)
+						} else if rsp != "OK" {
+							t.Errorf("[client %d]: Test call %d: got %q, want OK", i, j+1, rsp)
+						}
+					}
+				}()
+			}
+
+			// Wait for the clients to be finished and then close the listener so that
+			// the service loop will stop.
+			wg.Wait()
+			t.Log("Clients are finished; stopping listener")
+			lst.Close()
+			<-sc
+			t.Log("Server is finished")
+		})
+	}
 }
