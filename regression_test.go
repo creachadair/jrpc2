64f07be71b162a80d79260fb2371feecb95f7ad7
M. J. Fromberger <michael.j.fromberger@gmail.com>
2020-08-13 12:44:07 -0700

    Avoid logging after the test finishes.

--- regression_test.go
+++ regression_test.go
@@ -13,12 +13,12 @@ import (
 // Verify that a notification handler will not deadlock with the dispatcher on
 // holding the server lock. See: https://github.com/creachadair/jrpc2/pull/26
 func TestLockRaceRegression(t *testing.T) {
-	done := make(chan struct{})
+	hdone := make(chan struct{})
 	local := server.NewLocal(handler.Map{
 		// Do some busy-work and then try to get the server lock, in this case
 		// via the CancelRequest helper.
 		"Kill": handler.New(func(ctx context.Context, req *jrpc2.Request) error {
-			defer close(done) // signal we passed the deadlock point
+			defer close(hdone) // signal we passed the deadlock point
 
 			var id string
 			if err := req.UnmarshalParams(&handler.Args{&id}); err != nil {
@@ -38,19 +38,13 @@ func TestLockRaceRegression(t *testing.T) {
 
 	ctx := context.Background()
 	local.Client.Notify(ctx, "Kill", handler.Args{"1"})
-	go func() {
-		rsp, err := local.Client.Call(ctx, "Stall", nil)
-		if err != nil {
-			t.Logf("Call reported an error [expected]: %v", err)
-		} else {
-			t.Errorf("Call unexpectedly succeeded: %s", rsp.ResultString())
-		}
-	}()
+
+	go local.Client.Call(ctx, "Stall", nil)
 
 	select {
 	case <-time.After(10 * time.Second):
 		t.Fatal("Notification handler is probably deadlocked")
-	case <-done:
+	case <-hdone:
 		t.Log("Notification handler completed successfully")
 	}
 }
