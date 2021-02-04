bf8f042123fc10fa9db02e0e788a1c266ad2f387
M. J. Fromberger <michael.j.fromberger@gmail.com>
2021-02-04 10:36:47 -0800

    Rewrite context handlers in terms of ServerFromContext.

--- ctx.go
+++ ctx.go
@@ -11,9 +11,7 @@ import (
 // ServerMetrics returns the server metrics collector associated with the given
 // context, or nil if ctx does not have a collector attached.  The context
 // passed to a handler by *jrpc2.Server will include this value.
-func ServerMetrics(ctx context.Context) *metrics.M {
-	return ctx.Value(serverKey{}).(*Server).metrics
-}
+func ServerMetrics(ctx context.Context) *metrics.M { return ServerFromContext(ctx).metrics }
 
 // InboundRequest returns the inbound request associated with the given
 // context, or nil if ctx does not have an inbound request. The context passed
@@ -37,7 +35,7 @@ type inboundRequestKey struct{}
 // passed to the handler by *jrpc2.Server will support notifications if the
 // server was constructed with the AllowPush option set true.
 func PushNotify(ctx context.Context, method string, params interface{}) error {
-	return ctx.Value(serverKey{}).(*Server).Notify(ctx, method, params)
+	return ServerFromContext(ctx).Notify(ctx, method, params)
 }
 
 // PushCall posts a server call to the client. If ctx does not contain a server
@@ -48,7 +46,7 @@ func PushNotify(ctx context.Context, method string, params interface{}) error {
 // A successful callback reports a nil error and a non-nil response. Errors
 // returned by the client have concrete type *jrpc2.Error.
 func PushCall(ctx context.Context, method string, params interface{}) (*Response, error) {
-	return ctx.Value(serverKey{}).(*Server).Callback(ctx, method, params)
+	return ServerFromContext(ctx).Callback(ctx, method, params)
 }
 
 // CancelRequest requests the server associated with ctx to cancel the pending
