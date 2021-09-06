bce1c41352db85f74b38dc3c3fad126aad5790fe
M. J. Fromberger <michael.j.fromberger@gmail.com>
2021-09-05 19:28:41 -0700

    channel: Consolidate package files.
    Fold types.go into the main package file, which is hereby renamed from doc.go
    to channel.go.

--- channel/channel.go
+++ channel/channel.go
@@ -31,7 +31,11 @@
 //
 package channel
 
-import "strings"
+import (
+	"errors"
+	"io"
+	"strings"
+)
 
 // A Channel represents the ability to transmit and receive data records.  A
 // channel does not interpret the contents of a record, but may add and remove
@@ -60,4 +64,46 @@ func IsErrClosing(err error) bool {
 	// That we must check the string here appears to be working as intended, or at least
 	// there is no intent to make it better.  https://github.com/golang/go/issues/4373
 	return err != nil && strings.Contains(err.Error(), "use of closed network connection")
+}
+
+// A Framing converts a reader and a writer into a Channel with a particular
+// message-framing discipline.
+type Framing func(io.Reader, io.WriteCloser) Channel
+
+type direct struct {
+	send chan<- []byte
+	recv <-chan []byte
+}
+
+func (d direct) Send(msg []byte) (err error) {
+	cp := make([]byte, len(msg))
+	copy(cp, msg)
+	defer func() {
+		if p := recover(); p != nil {
+			err = errors.New("send on closed channel")
+		}
+	}()
+	d.send <- cp
+	return nil
+}
+
+func (d direct) Recv() ([]byte, error) {
+	msg, ok := <-d.recv
+	if ok {
+		return msg, nil
+	}
+	return nil, io.EOF
+}
+
+func (d direct) Close() error { close(d.send); return nil }
+
+// Direct returns a pair of synchronous connected channels that pass message
+// buffers directly in memory without framing or encoding. Sends to client will
+// be received by server, and vice versa.
+func Direct() (client, server Channel) {
+	c2s := make(chan []byte)
+	s2c := make(chan []byte)
+	client = direct{send: c2s, recv: s2c}
+	server = direct{send: s2c, recv: c2s}
+	return
 }
