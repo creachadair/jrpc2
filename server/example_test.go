ca93e3f3cda344e3ecc5cb46df29c7e363033825
M. J. Fromberger <michael.j.fromberger@gmail.com>
2023-03-14 13:23:44 -0700

    server: remove the Run helper
    This has no meaningful use to justify its existence in the API.
    
    Updates #46.

--- server/example_test.go
+++ server/example_test.go
@@ -8,7 +8,6 @@ import (
 	"log"
 
 	"github.com/creachadair/jrpc2"
-	"github.com/creachadair/jrpc2/channel"
 	"github.com/creachadair/jrpc2/handler"
 	"github.com/creachadair/jrpc2/server"
 )
@@ -46,21 +45,4 @@ func (Service) Assigner() (jrpc2.Assigner, error) {
 func (s Service) Finish(_ jrpc2.Assigner, stat jrpc2.ServerStatus) {
 	fmt.Printf("SERVICE FINISHED err=%v\n", stat.Err)
 	close(s.done)
-}
-
-func ExampleRun() {
-	done := make(chan struct{})
-	cch, sch := channel.Direct()
-	go server.Run(sch, Service{done}, nil)
-
-	cli := jrpc2.NewClient(cch, nil)
-	if _, err := cli.Call(context.Background(), "Hello", nil); err != nil {
-		log.Fatalf("Call failed: %v", err)
-	}
-	cli.Close()
-	<-done
-	// Output:
-	// SERVICE STARTED
-	// Hello human
-	// SERVICE FINISHED err=<nil>
 }
