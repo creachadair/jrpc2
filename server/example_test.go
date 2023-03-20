2c3680d0cfb83f633f126846b8bda9e33e171981
M. J. Fromberger <michael.j.fromberger@gmail.com>
2023-03-19 17:35:26 -0700

    server: remove dead example code

--- server/example_test.go
+++ server/example_test.go
@@ -7,7 +7,6 @@ import (
 	"fmt"
 	"log"
 
-	"github.com/creachadair/jrpc2"
 	"github.com/creachadair/jrpc2/handler"
 	"github.com/creachadair/jrpc2/server"
 )
@@ -27,22 +26,4 @@ func ExampleNewLocal() {
 	fmt.Println(result)
 	// Output:
 	// Hello, world!
-}
-
-// Service is a trivial service for testing purposes.
-type Service struct {
-	done chan struct{}
-}
-
-func (Service) Assigner() (jrpc2.Assigner, error) {
-	fmt.Println("SERVICE STARTED")
-	return handler.Map{"Hello": handler.New(func(ctx context.Context) error {
-		fmt.Println("Hello human")
-		return nil
-	})}, nil
-}
-
-func (s Service) Finish(_ jrpc2.Assigner, stat jrpc2.ServerStatus) {
-	fmt.Printf("SERVICE FINISHED err=%v\n", stat.Err)
-	close(s.done)
 }
